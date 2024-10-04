b831a5f8e4ec1b294ac5974d622939f48d0c366f
Arkeolo <152965884+Arkeolo@users.noreply.github.com>
2024-10-04 09:15:26 +0200

    Add test for getTask and getTasks

--- TaskManagementApi/obj/Debug/net8.0/win-x64/TaskManagementApi.AssemblyInfo.cs
+++ TaskManagementApi/obj/Debug/net8.0/win-x64/TaskManagementApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Incipientus_TM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae6dbb0ef9ba3e456a2f8fcec8554ca6fa6e0e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Incipientus_TM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Incipientus_TM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TaskManagementApp/src/app/services/task.service.spec.ts
+++ TaskManagementApp/src/app/services/task.service.spec.ts
@@ -1,6 +1,7 @@
 import { inject, TestBed } from '@angular/core/testing';
 import { HttpClientModule, HttpClient } from "@angular/common/http";
 import { TaskService } from './task.service';
+import { Task } from '../models/task';
 
 // Please complete the following integration tests for TaskService
 
@@ -25,6 +26,9 @@ describe('TaskService', () =>
     it('should return an array of tasks', (inject([HttpClient], async (http) =>
     {
       const service = new TaskService(http);
+      service.createTask(new Task(1, 'name', 'description', '10-10-24', '10-10-24', true, 'test'))
+      const tasks = service.getTasks();
+      expect(tasks).to.be.have.length.greaterThan(0);
       // TODO: Implement test
 
     })));
@@ -35,8 +39,8 @@ describe('TaskService', () =>
     it('should return a single task', (inject([HttpClient], async (http) =>
     {
       const service = new TaskService(http);
-      // TODO: Implement test
-
+      const task = service.getTask(1);
+      expect(task).be.true;
     })));
   });
 
